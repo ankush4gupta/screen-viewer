33fa1696369e4dd5e4ad2948ea613da0a4f7d45e
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2024-04-22 22:14:03 +0530

    added iframe and functionality for showing website

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,38 +1,62 @@
 import Options from "../options/page";
 import "./nav.css";
-export default function NavBar() {
+import { useState } from "react";
+interface NavbarProps {
+    onButtonClick: (height: string, width: string) => void;
+    onSubmit:any
+}
+export default function NavBar({ onButtonClick,onSubmit }: NavbarProps ) {
+    const [inputHeight, setHeight] = useState<string>('');
+    const [inputWidth, setWidth] = useState<string>('');
+    const [url, setUrl] = useState<string>('');
+    const handleButtonClick = (height: string, width: string) => {
+        onButtonClick(height, width);
+    };
+    // const handleHeightInput = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    //     event.preventDefault()
+    //     setHeight(event.target.value);
+    // }
+    // const handleWidthInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     event.preventDefault()
+    //      setWidth(event.target.value);
+    // }
+    const handleSubmit = () => {
+        onSubmit(url);
+    };
+
     return (
         <nav className="bg-sky-950 flex justify-items-center items-center">
+         {/* default screen options */}
             <div>
-                <Options name="Mobile" />
-                <Options name="Tab" />
-                <Options name="Desktop" />
-                <Options name="Ipad" />
+                <Options name="Mobile" onClick={()=> handleButtonClick('568','320')} />
+                <Options name="Tab" onClick={() => handleButtonClick('1024','768')} />
+                <Options name="Ipad" onClick={() => handleButtonClick('768','1024')} />
+                <Options name="Desktop" onClick={() => handleButtonClick('900','1440')} />
             </div>
-            <div className="mx-7 border-solid border-black border-2">
-                <input
-                    className="w-14 mx-4 rounded text-black-500 p-0.5"
-                    placeholder="height"
-                    type="text" />
+        {/* Custom screen setter */}
+            <div className="mx-7 border-solid border-black border-2">    
                 <input 
                     className="w-14 mx-4 rounded text-black-500 p-0.5"
                     placeholder="width"
-                type="text" />
-                <Options name="set" />
+                    type="text"
+                    onChange={(e) => setWidth(e.target.value)} />
+                <input
+                    className="w-14 mx-4 rounded text-black-500 p-0.5"
+                    placeholder="height"
+                    type="text"
+                    onChange={(e) => setHeight(e.target.value)} />
+                <button onClick={() => handleButtonClick(inputHeight, inputWidth)}>Set</button>   
             </div>
-            
+            {/* website search bar */}
            <div>
                 <input
                     className="ml-4 rounded text-black-500"
                     type="text"
                     placeholder="Enter website URL"
-                // value={""}
-                // onChange={(e) => setWebsiteUrl(e.target.value)}
+                    onChange={(e) => setUrl(e.target.value)}
                 />
-                <Options name="Search" />
+                <button onClick={handleSubmit}>Search</button>
            </div>
-            
-
         </nav>
     );
 }
--- src/app/options/page.tsx
+++ src/app/options/page.tsx
@@ -1,10 +1,11 @@
 import "./button.css";
 interface ButtonProps {
     name: string;
+    onClick: () => void;
 }
 
-export default function Options({ name }: ButtonProps) {
+export default function Options({ name,onClick }: ButtonProps) {
     return (
-        <button className="text-base text-red-50 bg-cyan-300 p-1.5 m-1.5 rounded">{name}</button>
+        <button onClick={onClick} className="text-base text-red-50 bg-cyan-300 p-1.5 m-1.5 rounded">{name}</button>
     );
 }
