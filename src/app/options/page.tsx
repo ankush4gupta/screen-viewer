82fd2ce7dba23e738ee9bc315c9ac8af54d5c915
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2024-04-23 23:35:59 +0530

    fix logo size and color

--- src/app/options/page.tsx
+++ src/app/options/page.tsx
@@ -1,7 +1,7 @@
 import { GoDeviceDesktop, GoDeviceMobile } from "react-icons/go";
 import { MdTabletAndroid } from "react-icons/md";
 import { IoIosPhonePortrait } from "react-icons/io";
-import { IoIosSearch } from "react-icons/io";
+
 import { TbDeviceIpad } from "react-icons/tb";
 import "./button.css";
 interface ButtonProps {
@@ -11,16 +11,16 @@ interface ButtonProps {
 
 export default function Options({ name,onClick }: ButtonProps) {
     const iconComponent: { [name: string]: any } = {
-        Desktop: <GoDeviceDesktop />,
-        Mobile: <IoIosPhonePortrait />,
-        Tab: <MdTabletAndroid />,
-        Ipad: <TbDeviceIpad />
+        Desktop: GoDeviceDesktop,
+        Mobile: IoIosPhonePortrait,
+        Tab: MdTabletAndroid,
+        Ipad: TbDeviceIpad
 
         // Add more mappings as needed
     };
     const IconComponent = iconComponent[name];
     return (
        
-        <button onClick={onClick} className="" > {IconComponent}</button > 
+        <button onClick={onClick} className="border-solid border-2 border-teal mx-5" > {<IconComponent size ="2.5rem" color="white"/>}</button > 
     );
 }
